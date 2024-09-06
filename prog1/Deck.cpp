a09d986cbfed6a896cc6bcff557d1d319af7ccc3
KevinVan562 <itiskevinvan@gmail.com>
2024-09-06 02:22:59 -0700

    added comment headers

--- prog1/Deck.cpp
+++ prog1/Deck.cpp
@@ -1,3 +1,11 @@
+// Name: Kevin Van
+// Class: CECS 325-02
+// Program 1 - One Card War
+// Due Date: 09/19/2024
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program
+
 #include "Deck.h"
 #include <iostream>
 #include <cstdlib>
